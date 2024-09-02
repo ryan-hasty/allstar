0b815693a71bf93023e1c255938cf63c0253397a
ryan-hasty <rhastydev@gmail.com>
2024-09-02 13:43:10 -0500

    small changes to footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,17 +20,17 @@ export default function RootLayout({
       <body>
       <Header></Header>
       <main>{children}</main>
-      <footer className="flex justify-start gap-20 sm:justify-between static bottom-0 right-0 p-4 bg-[#0F2E6C] border-none">
+          <footer className="flex justify-between md:gap-20 bottom-0 p-4 mt-10 bg-[#0F2E6C] border-none">
           <a
           href="https://www.facebook.com/profile.php?id=100077235862867"
           target={"_blank"}
-          className="bg-white ml-10">
-          <Image src="/assets/miscImages/facebook-logo.svg" alt="facebook-logo" width={32} height={32}></Image>
+          className="bg-white sm:ml-20">
+          <Image src="/assets/miscImages/facebook-logo.svg" alt="facebook-logo" width={34} height={34} className="h-auto"></Image>
           </a>
           <a
             href="https://www.linkedin.com/in/ryan-hasty/"
             target="_blank"
-            className="hover:text-[#F50000] text-xl text-white">
+            className="hover:text-[#F50000] text-xl text-white sm:mr-20">
           Created by Ryan Hasty
         </a>
       </footer>
