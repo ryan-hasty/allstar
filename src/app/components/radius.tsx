79d1eaccedbd4eb8644a3bfd3518822405275bc8
ryan-hasty <rhastydev@gmail.com>
2024-09-03 07:55:29 -0500

    move the service area up a little bit

--- src/app/components/radius.tsx
+++ src/app/components/radius.tsx
@@ -7,7 +7,7 @@ interface ServiceUrl{
 export default function Radius({ serviceRadiusUrl }: ServiceUrl){
 
     return (
-        <div className="flex justify-center mt-10 sm:mt-28">
+        <div className="flex justify-center mt-7 sm:mt-28">
             <Image src={serviceRadiusUrl || ""}  width={900} height={900} alt={"Service Radius" || ""}/>
         </div>
     )
